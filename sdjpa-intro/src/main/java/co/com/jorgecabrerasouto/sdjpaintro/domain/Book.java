6fedbfd781d9b6cc955a77f68bc544ecbce86557
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-08-03 12:27:24 -0500

    Vendor Specific Flyway Migrations

--- sdjpa-intro/src/main/java/co/com/jorgecabrerasouto/sdjpaintro/domain/Book.java
+++ sdjpa-intro/src/main/java/co/com/jorgecabrerasouto/sdjpaintro/domain/Book.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Book {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String title;
