13b764beb13872d94564494d81b493639c2cafde
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-08-12 23:35:34 -0500

    Initial commit for Hibernate DAO implementation

---
+++ sdjpa-jdbc-hibernate-dao/src/test/java/co/com/jorgecabrerasouto/jdbc/MySQLIntegrationTest.java
@@ -0,0 +1,27 @@
+package co.com.jorgecabrerasouto.jdbc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import co.com.jorgecabrerasouto.jdbc.repositories.BookRepository;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MySQLIntegrationTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testMySQL() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isGreaterThan(0);
+    }
+
+}
