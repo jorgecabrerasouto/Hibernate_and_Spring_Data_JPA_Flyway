828b7faa050127f1b53124fd1422e329b6937cbc
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-08-16 17:37:47 -0500

    Query Methods implemented

--- sdjpa-spring-data-jpa/src/main/java/co/com/jorgecabrerasouto/jdbc/repositories/AuthorRepository.java
+++ sdjpa-spring-data-jpa/src/main/java/co/com/jorgecabrerasouto/jdbc/repositories/AuthorRepository.java
@@ -5,4 +5,7 @@ import co.com.jorgecabrerasouto.jdbc.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+	
+	Author findAuthorByFirstNameAndLastName(String firstName, String lastName);
+	
 }
