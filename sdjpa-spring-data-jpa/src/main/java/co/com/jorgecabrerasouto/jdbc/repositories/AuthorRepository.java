9bb14fe0175615f3dcc8d9a0f7ff157659cd3f32
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-08-31 11:21:48 -0500

    Lesson 122. Query, Paging, and Sorting with Spring Data JPA. End of Section 14.

--- sdjpa-spring-data-jpa/src/main/java/co/com/jorgecabrerasouto/jdbc/repositories/AuthorRepository.java
+++ sdjpa-spring-data-jpa/src/main/java/co/com/jorgecabrerasouto/jdbc/repositories/AuthorRepository.java
@@ -3,6 +3,8 @@ package co.com.jorgecabrerasouto.jdbc.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.com.jorgecabrerasouto.jdbc.domain.Author;
@@ -10,5 +12,7 @@ import co.com.jorgecabrerasouto.jdbc.domain.Author;
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 	
 	Optional<Author> findAuthorByFirstNameAndLastName(String firstName, String lastName);
+	
+	Page<Author> findAuthorByLastName(String lastName, Pageable pageable);
 	
 }
