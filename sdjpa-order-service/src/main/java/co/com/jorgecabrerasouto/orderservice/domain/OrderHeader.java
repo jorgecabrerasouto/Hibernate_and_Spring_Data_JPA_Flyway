c867e5076ce76d8b94a76759948f48c4f1c2ec90
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-05 12:19:21 -0500

    Lesson 135. Association Helper Methods implemented.

--- sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/domain/OrderHeader.java
+++ sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/domain/OrderHeader.java
@@ -1,5 +1,6 @@
 package co.com.jorgecabrerasouto.orderservice.domain;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -63,6 +64,15 @@ public class OrderHeader extends BaseEntity {
     
     @OneToMany(mappedBy = "orderHeader", cascade = CascadeType.PERSIST)
     private Set<OrderLine> orderLines;
+    
+    public void addOrderLine (OrderLine orderLine) {
+    	if (orderLines == null) {
+    		orderLines = new HashSet<>();
+    	}
+    	
+    	orderLines.add(orderLine);
+    	orderLine.setOrderHeader(this);
+    }
 
     public String getCustomer() {
         return customer;
--- sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/domain/OrderLine.java
+++ sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/domain/OrderLine.java
@@ -1,7 +1,5 @@
 package co.com.jorgecabrerasouto.orderservice.domain;
 
-import java.util.Objects;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 
