7f6e26a54affdd65c2630cad8bcba86c9c325839
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-07 12:56:54 -0500

    Lessons 144. Load Test Data.

--- sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/repositories/CustomerRepository.java
+++ sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/repositories/CustomerRepository.java
@@ -1,8 +1,11 @@
 package co.com.jorgecabrerasouto.orderservice.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.com.jorgecabrerasouto.orderservice.domain.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	Optional<Customer> findCustomerByCustomerNameIgnoreCase(String customerName);
 }
--- sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/repositories/ProductRepository.java
+++ sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/repositories/ProductRepository.java
@@ -1,10 +1,12 @@
 package co.com.jorgecabrerasouto.orderservice.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.com.jorgecabrerasouto.orderservice.domain.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
-	Product findByDescription(String description); 
+	Optional<Product> findByDescription(String description); 
 }
