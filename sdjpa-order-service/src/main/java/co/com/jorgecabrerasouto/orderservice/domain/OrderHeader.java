f1c74e4f146af5c742ece3fcd393fd208e89ce4d
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-07 11:33:05 -0500

    Lesson 141. One to One Bi-Directional implemented.

--- sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/domain/OrderHeader.java
+++ sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/domain/OrderHeader.java
@@ -68,7 +68,7 @@ public class OrderHeader extends BaseEntity {
     @OneToMany(mappedBy = "orderHeader", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private Set<OrderLine> orderLines;
     
-    @OneToOne(cascade = CascadeType.PERSIST, orphanRemoval = true)
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, mappedBy = "orderHeader")
     private OrderApproval orderApproval;
     
     public OrderApproval getOrderApproval() {
@@ -77,6 +77,7 @@ public class OrderHeader extends BaseEntity {
 
 	public void setOrderApproval(OrderApproval orderApproval) {
 		this.orderApproval = orderApproval;
+		orderApproval.setOrderHeader(this);
 	}
 	
     public void addOrderLine (OrderLine orderLine) {
