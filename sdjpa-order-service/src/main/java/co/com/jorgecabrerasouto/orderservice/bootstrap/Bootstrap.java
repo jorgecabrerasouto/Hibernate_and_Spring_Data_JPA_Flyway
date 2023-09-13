c4b599651aada62be811f34f682603719bdbf787
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-13 16:42:02 -0500

    Lesson 152. Create Bootstrat Class.

---
+++ sdjpa-order-service/src/main/java/co/com/jorgecabrerasouto/orderservice/bootstrap/Bootstrap.java
@@ -0,0 +1,13 @@
+package co.com.jorgecabrerasouto.orderservice.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+	@Override
+	public void run (String... args) throws Exception {
+		System.out.println("I was called");
+	}
+}
