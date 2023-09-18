bb35e43ae98acc1a86976ae1c1b2389d984014c2
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-18 18:13:37 -0500

    Lesson 173. Initial Project Creation

---
+++ sdjpa-wp/src/main/java/co/com/jorgecabrerasouto/sdjpa/wp/SdjpaWpApplication.java
@@ -0,0 +1,13 @@
+package co.com.jorgecabrerasouto.sdjpa.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaWpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaWpApplication.class, args);
+	}
+
+}
