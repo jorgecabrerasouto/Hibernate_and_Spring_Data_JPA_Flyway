32c9350b35f2dc7161a9d36bea10918cfc79b49a
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-18 12:26:37 -0500

    Lesson 169. Single Table

---
+++ sdjpa-inheritance/src/main/java/co/com/jorgecabrerasouto/sdjpainheritance/domain/singletable/Car.java
@@ -0,0 +1,21 @@
+package co.com.jorgecabrerasouto.sdjpainheritance.domain.singletable;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("car")
+public class Car extends Vehicle {
+	
+	private String trimLevel;
+
+	public String getTrimLevel() {
+		return trimLevel;
+	}
+
+	public void setTrimLevel(String trimLevel) {
+		this.trimLevel = trimLevel;
+	}
+	
+
+}
