02b8983092c5c0d67e6f76508e28a10d4afbd941
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-18 11:30:55 -0500

    Lesson 166. Project Code Review

---
+++ sdjpa-inheritance/src/test/java/co/com/jorgecabrerasouto/orderservice/SdjpaInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.jorgecabrerasouto.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaInheritanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
