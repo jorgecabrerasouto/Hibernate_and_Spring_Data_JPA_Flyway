03ad790f66b5c9063afb5b392fcd54c6b7312ba0
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-19 18:58:25 -0500

    Lesson 186. Project Code Review.

---
+++ sdjpa-credit-card/src/main/java/co/com/jorgecabrerasouto/sdjpa/creditcard/SdjpaCreditCardApplication.java
@@ -0,0 +1,13 @@
+package co.com.jorgecabrerasouto.sdjpa.creditcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaCreditCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaCreditCardApplication.class, args);
+	}
+
+}
---
+++ sdjpa-credit-card/src/test/java/co/com/jorgecabrerasouto/sdjpa/creditcard/SdjpaCreditCardApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.jorgecabrerasouto.sdjpa.creditcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaCreditCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
