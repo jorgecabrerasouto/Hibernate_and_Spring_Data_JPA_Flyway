13d30d35eac7faf40eb971accb9a2716518123b8
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-19 19:27:14 -0500

    Lesson 187. Initial Persistence Test

---
+++ sdjpa-credit-card/src/test/java/co/com/jorgecabrerasouto/sdjpa/creditcard/repositories/CreditCardRepositoryTest.java
@@ -0,0 +1,38 @@
+package co.com.jorgecabrerasouto.sdjpa.creditcard.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import co.com.jorgecabrerasouto.sdjpa.creditcard.domain.CreditCard;
+
+@SpringBootTest
+@ActiveProfiles("local")
+@AutoConfigureTestDatabase (replace = AutoConfigureTestDatabase.Replace.NONE)
+class CreditCardRepositoryTest {
+	final String CREDIT_CARD = "12345678900000";
+	
+	@Autowired
+	CreditCardRepository creditCardRepository;
+	
+	@Test
+	void testSaveAndStoreCreditCard() {
+		CreditCard creditCard = new CreditCard();
+		creditCard.setCreditCardNumber(CREDIT_CARD);
+		creditCard.setCvv("123");
+		creditCard.setExpirationDate("12/2028");
+		
+		CreditCard savedCC = creditCardRepository.saveAndFlush(creditCard);
+		
+		System.out.println("Getting CC from database");
+		
+		CreditCard fetchedCC = creditCardRepository.findById(savedCC.getId()).get();
+		
+		assertThat(savedCC.getCreditCardNumber()).isEqualTo(fetchedCC.getCreditCardNumber());
+	}
+
+}
