ec2fe0f3595a99252c8e6b647627448efbe63ca2
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-21 12:42:58 -0500

    Lesson 190. Verify Data at Rest.

--- sdjpa-credit-card/src/test/java/co/com/jorgecabrerasouto/sdjpa/creditcard/repositories/CreditCardRepositoryTest.java
+++ sdjpa-credit-card/src/test/java/co/com/jorgecabrerasouto/sdjpa/creditcard/repositories/CreditCardRepositoryTest.java
@@ -2,10 +2,13 @@ package co.com.jorgecabrerasouto.sdjpa.creditcard.repositories;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import co.com.jorgecabrerasouto.sdjpa.creditcard.domain.CreditCard;
@@ -23,6 +26,9 @@ class CreditCardRepositoryTest {
 	@Autowired
 	CreditCardRepository creditCardRepository;
 	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
 	@Test
 	void testSaveAndStoreCreditCard() {
 		CreditCard creditCard = new CreditCard();
@@ -36,6 +42,14 @@ class CreditCardRepositoryTest {
 		
 		System.out.println("CC at Rest");
 		System.out.println("CC Encrypted: " + encryptionService.encrypt(CREDIT_CARD));
+		
+		Map<String, Object> dbRow = jdbcTemplate.queryForMap("SELECT * FROM credit_card " +
+		" WHERE id = " + savedCC.getId());
+		
+		String dbCardValue = (String) dbRow.get("credit_card_number");
+		
+		assertThat(savedCC.getCreditCardNumber()).isNotEqualTo(dbCardValue);
+		assertThat(dbCardValue).isEqualTo(encryptionService.encrypt(CREDIT_CARD));
 		
 		CreditCard fetchedCC = creditCardRepository.findById(savedCC.getId()).get();
 		
