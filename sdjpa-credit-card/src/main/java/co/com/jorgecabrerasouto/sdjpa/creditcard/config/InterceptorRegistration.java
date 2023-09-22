b6645649f6495120d72a818083c1fed0683e86fb
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-22 11:52:29 -0500

    Lesson 190. Create Hibernate Interceptor.

---
+++ sdjpa-credit-card/src/main/java/co/com/jorgecabrerasouto/sdjpa/creditcard/config/InterceptorRegistration.java
@@ -0,0 +1,21 @@
+package co.com.jorgecabrerasouto.sdjpa.creditcard.config;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+import co.com.jorgecabrerasouto.sdjpa.creditcard.interceptors.EncryptionInterceptor;
+
+@Configuration
+public class InterceptorRegistration implements HibernatePropertiesCustomizer {
+
+    @Autowired
+    EncryptionInterceptor interceptor;
+
+    @Override
+    public void customize(Map<String, Object> hibernateProperties) {
+        hibernateProperties.put("hibernate.session_factory.interceptor", interceptor);
+    }
+}
---
+++ sdjpa-credit-card/src/main/java/co/com/jorgecabrerasouto/sdjpa/creditcard/interceptors/EncryptionInterceptor.java
@@ -0,0 +1,32 @@
+package co.com.jorgecabrerasouto.sdjpa.creditcard.interceptors;
+
+import java.io.Serializable;
+
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.type.Type;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EncryptionInterceptor extends EmptyInterceptor {
+    @Override
+    public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
+
+        System.out.println("I'm in onFlushDirty");
+
+        return super.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+    }
+
+    @Override
+    public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+        System.out.println("I'm in onLoad");
+
+        return super.onLoad(entity, id, state, propertyNames, types);
+    }
+
+    @Override
+    public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+        System.out.println("I'm in onSave");
+
+        return super.onSave(entity, id, state, propertyNames, types);
+    }
+}
