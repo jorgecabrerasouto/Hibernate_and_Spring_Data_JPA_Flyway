80116a2b9014224ea4ef379927078ffda3c1e5db
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-25 11:57:55 -0500

    Lesson 200. JPA Callback on Entity Methods

--- sdjpa-credit-card/src/main/java/co/com/jorgecabrerasouto/sdjpa/creditcard/domain/CreditCard.java
+++ sdjpa-credit-card/src/main/java/co/com/jorgecabrerasouto/sdjpa/creditcard/domain/CreditCard.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 
 @Entity
 public class CreditCard {
@@ -50,5 +51,10 @@ public class CreditCard {
 
     public void setExpirationDate(String expirationDate) {
         this.expirationDate = expirationDate;
+    }
+    
+    @PrePersist
+    public void prePersistCallback() {
+    	System.out.println("JPA PrePersist Callback was called");
     }
 }
