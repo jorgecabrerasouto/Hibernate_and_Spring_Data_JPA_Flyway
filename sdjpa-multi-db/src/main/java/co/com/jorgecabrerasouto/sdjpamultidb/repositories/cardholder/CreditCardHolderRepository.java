019c1ace6863b71b208a29b4842b6b34f37b3659
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-27 22:38:19 -0500

    Lesson 224. Get Credit Card By Id

--- sdjpa-multi-db/src/main/java/co/com/jorgecabrerasouto/sdjpamultidb/repositories/cardholder/CreditCardHolderRepository.java
+++ sdjpa-multi-db/src/main/java/co/com/jorgecabrerasouto/sdjpamultidb/repositories/cardholder/CreditCardHolderRepository.java
@@ -1,9 +1,12 @@
 package co.com.jorgecabrerasouto.sdjpamultidb.repositories.cardholder;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.com.jorgecabrerasouto.sdjpamultidb.domain.cardholder.CreditCardHolder;
 
 public interface CreditCardHolderRepository extends JpaRepository<CreditCardHolder, Long> {
 
+	Optional<CreditCardHolder> findByCreditCardId(Long id);
 }
--- sdjpa-multi-db/src/main/java/co/com/jorgecabrerasouto/sdjpamultidb/repositories/pan/CreditCardPANRepository.java
+++ sdjpa-multi-db/src/main/java/co/com/jorgecabrerasouto/sdjpamultidb/repositories/pan/CreditCardPANRepository.java
@@ -1,8 +1,12 @@
 package co.com.jorgecabrerasouto.sdjpamultidb.repositories.pan;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import co.com.jorgecabrerasouto.sdjpamultidb.domain.cardholder.CreditCardHolder;
 import co.com.jorgecabrerasouto.sdjpamultidb.domain.pan.CreditCardPAN;
 
 public interface CreditCardPANRepository extends JpaRepository<CreditCardPAN, Long> {
+	Optional<CreditCardPAN> findByCreditCardId(Long id);
 }
