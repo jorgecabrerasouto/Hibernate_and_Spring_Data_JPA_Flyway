bb232fce491a1ea2d77451e3a8521c96f6ee3499
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-28 15:55:10 -0500

    Initial project add

---
+++ sdjpa-spring-data-rest/src/main/java/co/com/jorgecabrerasouto/sfgrestbrewery/SdjpaSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package co.com.jorgecabrerasouto.sfgrestbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaSpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaSpringDataRestApplication.class, args);
+	}
+
+}
---
+++ sdjpa-spring-data-rest/src/test/java/co/com/jorgecabrerasouto/sfgrestbrewery/SdjpaSpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.jorgecabrerasouto.sfgrestbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaSpringDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
