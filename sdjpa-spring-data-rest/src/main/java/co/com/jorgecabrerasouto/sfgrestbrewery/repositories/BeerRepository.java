6a03cc42ed4c97f31d28e96636112856a9da25b6
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-09-28 16:26:58 -0500

    Lesson 231. Customize URL path

--- sdjpa-spring-data-rest/src/main/java/co/com/jorgecabrerasouto/sfgrestbrewery/repositories/BeerRepository.java
+++ sdjpa-spring-data-rest/src/main/java/co/com/jorgecabrerasouto/sfgrestbrewery/repositories/BeerRepository.java
@@ -5,10 +5,12 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import co.com.jorgecabrerasouto.sfgrestbrewery.domain.Beer;
 import co.com.jorgecabrerasouto.sfgrestbrewery.domain.BeerStyleEnum;
 
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 	
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
